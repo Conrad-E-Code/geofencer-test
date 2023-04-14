0d6df6f205a6359fe15080e1479c5f0d499273ca
Conrad-E-Code <112827066+Conrad-E-Code@users.noreply.github.com>
2023-04-13 22:52:21 -0600

    styling with flex-box, simple textinput

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Goal"/>
+        <Button title="add goal"/>
+      </View>
+      <View>
+        <Text>Your list of goals...</Text>
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: 'space-between'
+
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    width: '80%',
+    marginRight: 8
+  }
+
 });
+
+// container: {
+//   flex: 1,
+//   backgroundColor: '#fff',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// },
+// customText: {
+//   color: "blue",
+//   borderWidth: 5,
+//   borderColor: "red",
+//   padding: 20,
+//   margin: 20,
+//   borderRadius: 7
+// }
